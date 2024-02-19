24f9aae289a769637ee061194b7f9e485252cddf
John Scolaro <johnscolaro95@gmail.com>
2024-02-19 20:44:41 +1000

    Change recipe page to say recipe, not blog.

--- app/(topbarlayout)/recipes/page.tsx
+++ app/(topbarlayout)/recipes/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
       <section>
         <div className="max-w-6xl p-4 m-auto">
           <div className="mt-2 md:mt-4 font-bold text-3xl text-center">
-            Blog
+            Recipes
           </div>
           <div className="flex flex-col items-center mt-4 gap-4">
             {allRecipeData.map(({ id, title }) => (
